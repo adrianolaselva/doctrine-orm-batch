133d3beb8a3d4f3d2c44562436705973be60019b
Adriano M. La Selva <adrianolaselva@gmail.com>
2016-07-16 11:24:02 -0300

    Correção de testes

--- tests/CIELO/WorkerTest.php
+++ tests/CIELO/WorkerTest.php
@@ -36,12 +36,13 @@ class WorkerTest extends PHPUnit_Framework_TestCase
 
     public function testWorkerExtratoAntRecebiveis()
     {
-        $processed = WorkerTest::PATH_BASE . getenv("edi.proccessed") . DIRECTORY_SEPARATOR . 'cielo' . DIRECTORY_SEPARATOR . '3a2df08ce2e98441cad7b341efee2275d234adeb';
+        $filesTest = WorkerTest::PATH_BASE . getenv("edi.pending") . DIRECTORY_SEPARATOR . 'cielo_test' . DIRECTORY_SEPARATOR . '3a2df08ce2e98441cad7b341efee2275d234adeb';
         $pending =  WorkerTest::PATH_BASE . getenv("edi.pending") . DIRECTORY_SEPARATOR . 'cielo' . DIRECTORY_SEPARATOR . '3a2df08ce2e98441cad7b341efee2275d234adeb';
 
         try{
-            if(is_file($processed)){
-                $this->assertTrue(rename($processed,$pending), "Falha ao mover o arquivo para pasta pending");
+            $this->assertTrue(is_file($filesTest),"Arquivo não encontrado p/ realizar testes");
+            if(is_file($filesTest)){
+                $this->assertTrue(copy($filesTest,$pending), "Falha ao mover o arquivo para pasta pending");
             }
             $this->worker->run();
             $this->assertTrue(true);
@@ -52,12 +53,13 @@ class WorkerTest extends PHPUnit_Framework_TestCase
 
     public function testWorkerCVParceladoFuturo()
     {
-        $processed = WorkerTest::PATH_BASE . getenv("edi.proccessed") . DIRECTORY_SEPARATOR . 'cielo' . DIRECTORY_SEPARATOR . '58c005fa1abffa8518c10770e6259f215b07ed8a';
+        $filesTest = WorkerTest::PATH_BASE . getenv("edi.pending") . DIRECTORY_SEPARATOR . 'cielo_test' . DIRECTORY_SEPARATOR . '58c005fa1abffa8518c10770e6259f215b07ed8a';
         $pending =  WorkerTest::PATH_BASE . getenv("edi.pending") . DIRECTORY_SEPARATOR . 'cielo' . DIRECTORY_SEPARATOR . '58c005fa1abffa8518c10770e6259f215b07ed8a';
 
         try{
-            if(is_file($processed)){
-                $this->assertTrue(rename($processed,$pending), "Falha ao mover o arquivo para pasta pending");
+            $this->assertTrue(is_file($filesTest),"Arquivo não encontrado p/ realizar testes");
+            if(is_file($filesTest)){
+                $this->assertTrue(copy($filesTest,$pending), "Falha ao mover o arquivo para pasta pending");
             }
             $this->worker->run();
             $this->assertTrue(true);
@@ -68,12 +70,13 @@ class WorkerTest extends PHPUnit_Framework_TestCase
 
     public function testWorkerCVParceladoFuturo2()
     {
-        $processed = WorkerTest::PATH_BASE . getenv("edi.proccessed") . DIRECTORY_SEPARATOR . 'cielo' . DIRECTORY_SEPARATOR . 'fae9ecf9f4afe446c745613bec59a022e03a4fb9';
+        $filesTest = WorkerTest::PATH_BASE . getenv("edi.pending") . DIRECTORY_SEPARATOR . 'cielo_test' . DIRECTORY_SEPARATOR . 'fae9ecf9f4afe446c745613bec59a022e03a4fb9';
         $pending =  WorkerTest::PATH_BASE . getenv("edi.pending") . DIRECTORY_SEPARATOR . 'cielo' . DIRECTORY_SEPARATOR . 'fae9ecf9f4afe446c745613bec59a022e03a4fb9';
 
         try{
-            if(is_file($processed)){
-                $this->assertTrue(rename($processed,$pending), "Falha ao mover o arquivo para pasta pending");
+            $this->assertTrue(is_file($filesTest),"Arquivo não encontrado p/ realizar testes");
+            if(is_file($filesTest)){
+                $this->assertTrue(copy($filesTest,$pending), "Falha ao mover o arquivo para pasta pending");
             }
             $this->worker->run();
             $this->assertTrue(true);
@@ -81,5 +84,7 @@ class WorkerTest extends PHPUnit_Framework_TestCase
             $this->assertTrue(false, $ex->getMessage());
         }
     }
+
+
 
 }
